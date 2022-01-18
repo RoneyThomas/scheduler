4ae868390bcefd55aa2be3e5c80579effc65c44f
RoneyThomas <roneythomas6@gmail.com>
2022-01-18 15:32:52 -0500

    Refactored

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -10,10 +10,10 @@ export default function InterviewerList(props) {
       <ul className="interviewers__list">
         {props.interviewers.map(interviewer => (
           <InterviewerListItem
-            id={interviewer.id}
+            key={interviewer.id}
             name={interviewer.name}
             avatar={interviewer.avatar}
-            setInterviewer={props.setInterviewer}
+            setInterviewer={() => props.setInterviewer(interviewer.id)}
             selected={props.interviewer ? props.interviewer === interviewer.id : undefined}
           />
         ))}
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,20 +4,18 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
-  const InterviewerListItemClass = classNames({
+  const interviewerClass = classNames({
     "interviewers__item": !props.selected,
     "interviewers__item--selected": props.selected
   });
-  console.log(props);
-  console.log(InterviewerListItemClass);
   return (
-    <li className={InterviewerListItemClass} onClick={props.setInterviewer ? () => props.setInterviewer(props.id) : undefined}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   )
 }
